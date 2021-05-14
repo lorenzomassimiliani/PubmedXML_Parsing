66432f1c3628860f0d86ad69e15ba74de6f52789
lorenzo <massimilianilorenzo@gmail.com>
2021-05-14 16:39:54 +0200

    Added sql queries to create tables

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,36 @@ import java.sql.Statement;
 
 public class Main {
 
+    public static Connection GetConnection() throws SQLException {
+        String ConnectionString = "jdbc:postgresql://ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/d8dsa8l6mtacb3";
+        String username = "vgisptadjxxnpl";
+        String password = "c79c309b2211b97bae7748d2c9b0f586c9c7d4dbe5db94acb8c850e0b86c7d64";
+        return DriverManager.getConnection(ConnectionString, username, password);
+    }
 
-    public static void main(String[] args) {
-
-        try{
-            Connection connection = dataSource();
+    public static void ExecuteQuery(String query) {
+        try {
+            Connection connection = GetConnection();
             Statement stmt = connection.createStatement();
-            System.out.println("conected  ");
-
-
-        String sql = "INSERT INTO registration   "  +
-              "VALUES  (12, 14, 1, 15);  " ;
-
-            stmt.executeUpdate(sql);
-        }
-
-        catch(Exception ex){
+            System.out.println("Connected");
+            stmt.execute(query);
+            System.out.println("Query completed");
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static Connection dataSource() throws SQLException {
 
-        String ConnectionString = "jdbc:postgresql://ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/dd0of0uufj69pe";
-        String username = "kygmxpoanyhdim";
-        String password = "89079527e60afab5bd7969a366c9771f4778289627b619e00f04b9de81649aeb";
-        return DriverManager.getConnection(ConnectionString, username, password);
+    public static void main(String[] args) {
+
+        String sql = "CREATE TABLE UMLS (" +
+                "UMLSID INT PRIMARY KEY NOT NULL, " +
+                "ConceptPreferredName Varchar, " +
+                "ConceptUniqueIdentifier Varchar, " +
+                "SemanticType Varchar); ";
+
+        ExecuteQuery(sql);
+
     }
 
 }
